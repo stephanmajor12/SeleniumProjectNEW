fbcb71eec1d2e1d51251cad0dff041010fe03bc2
Akbar <akbar@cybertekschool.com>
2021-09-07 15:59:35 -0400

    Day 04 : XPath intro : absolute xpath

---
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/FindElementByXPath_Intro.java
@@ -0,0 +1,28 @@
+package com.cybertek.day04_common_elements_xpath_css_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementByXPath_Intro {
+
+    public static void main(String[] args) {
+
+        //  inspect element --> right click --> copy --> copy full xpath
+        //          /html/body/div/div[2]/div/div/div/h1/span
+        // XPath can be used to navigate through elements and attributes in a html document.
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver() ;
+        driver.get("http://practice.cybertekschool.com");
+        // using above full xpath to identify the element and get the text
+        WebElement headerSpan = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/h1/span"));
+        System.out.println("headerSpan.getText() = " + headerSpan.getText());
+
+
+    }
+
+}
