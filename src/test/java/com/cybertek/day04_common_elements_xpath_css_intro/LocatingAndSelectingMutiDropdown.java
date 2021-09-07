3f1d0dbb02a69bec8c039fe3a63dfceadb1a1bbf
Akbar <akbar@cybertekschool.com>
2021-09-07 12:51:15 -0400

    Day 04 : Working with dropdown list multi item completed

--- src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingAndSelectingMutiDropdown.java
+++ src/test/java/com/cybertek/day04_common_elements_xpath_css_intro/LocatingAndSelectingMutiDropdown.java
@@ -1,8 +1,11 @@
 package com.cybertek.day04_common_elements_xpath_css_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LocatingAndSelectingMutiDropdown {
 
@@ -14,6 +17,28 @@ public class LocatingAndSelectingMutiDropdown {
 
         driver.get("http://practice.cybertekschool.com/dropdown");
 
+        // TODO : Identify language multi select dropdown
+        WebElement langDropdownElm = driver.findElement(By.name("Languages"));
+        // TODO: Wrap it up using Select class so we can use easy methods
+        Select langSelectObj = new Select(langDropdownElm) ;
+        // TODO : call all available methods
+
+        System.out.println("langSelectObj.isMultiple() = " + langSelectObj.isMultiple());
+
+        langSelectObj.selectByIndex(2);
+        langSelectObj.selectByValue("c");
+        langSelectObj.selectByVisibleText("Java");
+        // at this moment 3 options are selected at the same time
+
+        // now deselect
+        langSelectObj.deselectByIndex(2);
+        langSelectObj.deselectByValue("c");
+        langSelectObj.deselectByVisibleText("Java");
+
+        // deselect all
+        langSelectObj.deselectAll();
+
+        driver.quit();
 
     }
 
