2b286b572cceb57a3a74de67c36159e69157ba3e
Akbar <akbar@cybertekschool.com>
2021-09-13 13:09:44 -0400

    Day 05 : XPath Practice

---
+++ src/test/java/com/cybertek/day05_css_xpath_junit5/XPathPractice1.java
@@ -0,0 +1,39 @@
+package com.cybertek.day05_css_xpath_junit5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPathPractice1 {
+
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+
+        // locate 2 elements using xpath
+        WebElement forgetPasswordHeader = driver.findElement(By.xpath("//h2[ text() = 'Forgot Password']"));
+        System.out.println("forgetPasswordHeader.getText() = " + forgetPasswordHeader.getText());
+
+        WebElement emailBox = driver.findElement(By.xpath("//input[ @name='email' and @type='text']")) ;
+        emailBox.sendKeys("someone@somewhere.com");
+
+
+        WebElement retrieveBtn = driver.findElement(By.xpath("//button[@id='form_submit' and @class='radius']"));
+        retrieveBtn.click();
+
+        // use xpath to identify confirmation message
+        //Your e-mail's been sent!
+        //h4[@name='confirmation_message' and text()='Your e-mail's been sent!']
+        WebElement confirmationMsg = driver.findElement(By.xpath("//h4[@name='confirmation_message' and text()=\"Your e-mails been sent!\"]")) ;
+        System.out.println("confirmationMsg.getText() = " + confirmationMsg.getText());
+
+
+    }
+
+}
