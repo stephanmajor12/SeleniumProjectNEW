f12acda5f97c61d2428a02bbba81b4a586056cd9
Akbar <akbar@cybertekschool.com>
2021-09-14 14:58:03 -0400

    Day 06 : Implicit wait example for slow elements that does not show up immediately

---
+++ src/test/java/com/cybertek/tests/day06_junit_practice_utiliy_methods/SlowElementTest.java
@@ -0,0 +1,32 @@
+package com.cybertek.tests.day06_junit_practice_utiliy_methods;
+
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class SlowElementTest extends TestBase {
+
+    @Test
+    public void testWait() throws InterruptedException {
+
+
+        // we can instruct WebDriver to wait maximum amount of time
+        // before throwing NoSuchElement Exception by using implicit wait
+        // If element is not available , it will wait for maximum of 10second
+        // If element is found before 10s for example 1 second - > It will move on to next step
+
+        driver.navigate().to("http://practice.cybertekschool.com/dynamic_controls");
+        driver.findElement(By.xpath("//button[ .='Remove']")).click();
+
+//        Thread.sleep(5000);
+
+        WebElement itsGoneElm = driver.findElement(By.id("message")) ;
+        System.out.println("itsGoneElm.getText() = " + itsGoneElm.getText());
+        System.out.println("THE END ");
+
+    }
+
+}
