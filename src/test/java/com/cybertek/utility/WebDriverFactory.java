3af56b563e6fd667357cc221a12dc350678c3828
Akbar <akbar@cybertekschool.com>
2021-09-14 14:29:21 -0400

    Day 06 : Added toLowerCase() to make it case- insensitive

--- src/test/java/com/cybertek/utility/WebDriverFactory.java
+++ src/test/java/com/cybertek/utility/WebDriverFactory.java
@@ -21,7 +21,7 @@ public class WebDriverFactory {
 
         WebDriver driver ;
 
-        switch (browserName){
+        switch (browserName.toLowerCase() ){
             case "chrome" :
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
