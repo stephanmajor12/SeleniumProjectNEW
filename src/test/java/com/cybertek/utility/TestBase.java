d9faffb1668fd0474514d109a0c506a1335631f5
Akbar <akbar@cybertekschool.com>
2021-09-14 15:02:41 -0400

    Day 06 : Implicit wait example for slow elements that does not show up immediately (note)

--- src/test/java/com/cybertek/utility/TestBase.java
+++ src/test/java/com/cybertek/utility/TestBase.java
@@ -21,6 +21,10 @@ public abstract class TestBase {
 //        driver = new ChromeDriver();
 //        driver.manage().window().maximize();
         driver = WebDriverFactory.getDriver("chrome");
+        // This is how we can set maximum timeout for finding element
+        // in this example it will wait for 10 seconds
+        // if element is found in 1 second ,it will just move on without finishing 10 seconds
+        // Thread.sleep(100000) will always wait for 10 seconds no matter what.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
     }
 
