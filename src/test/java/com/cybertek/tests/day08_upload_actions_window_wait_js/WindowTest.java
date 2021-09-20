c61433da295eccf3584dcec77f33308138dda76a
Akbar <akbar@cybertekschool.com>
2021-09-20 14:56:08 -0400

    Day 08 : Working with windows starter getWindowHandle(s)

---
+++ src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/WindowTest.java
@@ -0,0 +1,30 @@
+package com.cybertek.tests.day08_upload_actions_window_wait_js;
+
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+public class WindowTest extends TestBase {
+
+    @Test
+    public void testWindowOrTab(){
+
+        // selenium see window or tab with no difference
+        // everything tab is new window for selenium
+        driver.get("http://practice.cybertekschool.com/windows");
+
+        // save windowHandle of this tab into currentHandle variable
+        String currentHandle = driver.getWindowHandle();
+        System.out.println("currentHandle = " + currentHandle);
+
+        // try to use getWindowHandles() method and print it out
+        Set<String> allHandles = driver.getWindowHandles();
+        System.out.println("allHandles = " + allHandles);
+
+
+
+
+    }
+
+}
