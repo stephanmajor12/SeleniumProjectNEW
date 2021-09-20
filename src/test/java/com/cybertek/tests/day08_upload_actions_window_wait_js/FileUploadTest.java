4057e3634cc477769a6c84fe6ba10c738ac9e5e7
Akbar <akbar@cybertekschool.com>
2021-09-20 10:25:36 -0400

    Day 08 : File upload started

---
+++ src/test/java/com/cybertek/tests/day08_upload_actions_window_wait_js/FileUploadTest.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day08_upload_actions_window_wait_js;
+
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class FileUploadTest extends TestBase {
+
+    @Test
+    public void testUpload(){
+
+        driver.get("http://practice.cybertekschool.com/upload");
+
+    }
+
+}
