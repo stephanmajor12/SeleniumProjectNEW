234228c1973254fb6d849a1da92fa378e8357564
Akbar <akbar@cybertekschool.com>
2021-09-20 11:11:34 -0400

    Day 08 : BrowserUtil class with waitFor method

---
+++ src/test/java/com/cybertek/utility/BrowserUtil.java
@@ -0,0 +1,19 @@
+package com.cybertek.utility;
+
+public class BrowserUtil {
+
+    /**
+     * A method to pause the thread certain seconds
+     * @param seconds
+     */
+    public static void waitFor(int seconds){
+
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
