81f656c6b31f8086a238036a8c0fe92edb7f5a5a
Akbar <akbar@cybertekschool.com>
2021-09-21 16:06:38 -0400

    Day 09 : try out Singleton class

---
+++ src/test/java/com/cybertek/tests/day09_explict_wait_singleton_properties/SingletonPractice.java
@@ -0,0 +1,36 @@
+package com.cybertek.tests.day09_explict_wait_singleton_properties;
+
+import org.junit.jupiter.api.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void testSingleton(){
+
+
+//        Singleton s1 = new Singleton();
+//        s1.word = "abc" ;
+//
+//        Singleton s2 = new Singleton();
+//        s2.word = "efg" ;
+
+        Singleton.getObj();
+        Singleton.getObj();
+        Singleton.getObj();
+        Singleton.getObj();
+        Singleton.getObj();
+        /**
+         * This is the output we got
+         * Object has not been created yet, creating one now
+         * You already have object , use that existing object
+         * You already have object , use that existing object
+         * You already have object , use that existing object
+         * You already have object , use that existing object
+         */
+
+
+
+    }
+
+
+}
