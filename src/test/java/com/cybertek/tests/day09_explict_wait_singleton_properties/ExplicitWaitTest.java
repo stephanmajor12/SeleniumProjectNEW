850bd49af801b854dd257bb4f40796cc5b403dd9
Akbar <akbar@cybertekschool.com>
2021-09-21 11:27:02 -0400

    Day 09 : Explicit Wait Starter

---
+++ src/test/java/com/cybertek/tests/day09_explict_wait_singleton_properties/ExplicitWaitTest.java
@@ -0,0 +1,23 @@
+package com.cybertek.tests.day09_explict_wait_singleton_properties;
+
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class ExplicitWaitTest extends TestBase {
+
+
+    @Test
+    public void testWaitForTitle(){
+
+        //1. navigate to http://practice.cybertekschool.com/dynamic_loading
+
+        //2. Click on Example 7
+
+        //3. The title will show loading... until certain time ,
+        // we want to wait until the title value become "Dynamic Title"
+
+    }
+
+
+
+}
