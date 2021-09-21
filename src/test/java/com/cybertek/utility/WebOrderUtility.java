cf085a29fe0c1d579b4cf58fc304e23aca8f864a
Akbar <akbar@cybertekschool.com>
2021-09-21 13:03:42 -0400

    Day 09 : WebOrderUtility login method

---
+++ src/test/java/com/cybertek/utility/WebOrderUtility.java
@@ -0,0 +1,20 @@
+package com.cybertek.utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class WebOrderUtility {
+
+
+    public static void login(WebDriver driver){
+
+        // enter username
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        // enter password
+        driver.findElement(By.id("ctl00_MainContent_password")) .sendKeys("test");
+        // click login
+        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+
+    }
+
+}
