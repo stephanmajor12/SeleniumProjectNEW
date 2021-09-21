bd96087f2f1e16b7a31627a71fdddc4ac3202741
Akbar <akbar@cybertekschool.com>
2021-09-21 14:26:06 -0400

    Day 09 : Using WebOrderUtility login method

--- src/test/java/com/cybertek/utility/WebOrderUtility.java
+++ src/test/java/com/cybertek/utility/WebOrderUtility.java
@@ -3,17 +3,21 @@ package com.cybertek.utility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+// THIS AIN'T NO TEST CLASS SO WE CAN NOT EXTEND TESTBASE
+// IT SIMPLY DOES NOT MAKE SENSE
 public class WebOrderUtility {
 
 
-    public static void login(WebDriver driver){
+    public static void login(WebDriver driverParam){
 
+        // BELOW LINE WILL NOT WORK BECAUSE IT WILL OPEN NEW DRIVER EACH TIME
+//        WebDriver driver = WebDriverFactory.getDriver("chrome");
         // enter username
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driverParam.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         // enter password
-        driver.findElement(By.id("ctl00_MainContent_password")) .sendKeys("test");
+        driverParam.findElement(By.id("ctl00_MainContent_password")) .sendKeys("test");
         // click login
-        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+        driverParam.findElement(By.id("ctl00_MainContent_login_button")).click();
 
     }
 
