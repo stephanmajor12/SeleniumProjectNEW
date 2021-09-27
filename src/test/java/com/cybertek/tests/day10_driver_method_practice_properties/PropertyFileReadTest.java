9f6e064781e6a9b9f8af30f1f71b515ab029ec69
Akbar <akbar@cybertekschool.com>
2021-09-27 15:51:36 -0400

    Day 10 : Reading from config.properties

---
+++ src/test/java/com/cybertek/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
@@ -0,0 +1,62 @@
+package com.cybertek.tests.day10_driver_method_practice_properties;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileReadTest {
+
+    @Test
+    public void testInitialRead() throws IOException {
+
+        // Open a connection to the file using FileInputStream object
+        FileInputStream in = new FileInputStream("config.properties") ;
+
+        // Create empty Properties object
+        Properties myProperty = new Properties() ;
+        // Load the FileInputStream into the Properties Object
+        myProperty.load(in);
+        // close the connection by calling close method of FileInputStream object
+        in.close();
+
+        // now actually read from the properties file using it's key
+        String helloValue =  myProperty.getProperty("hello");
+        System.out.println("helloValue = " + helloValue);
+
+        // read the value of key called "message"
+        System.out.println("myProperty.getProperty(\"message\") = "
+                + myProperty.getProperty("message"));
+
+        System.out.println("myProperty.getProperty(\"weborder_url\") = "
+                + myProperty.getProperty("weborder_url"));
+
+
+    }
+
+
+    @Test
+    public void testReadWithTryCatch(){
+
+
+        try {
+            // this throw FileNotFoundException
+            FileInputStream in = new FileInputStream("config.properties") ;
+
+            Properties properties = new Properties();
+            properties.load(in);
+            // this throw IOException , it's parent of FileNotFoundException
+            in.close();
+            System.out.println("properties.getProperty(\"hello\") = "
+                    + properties.getProperty("hello"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
