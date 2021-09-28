ddb5523ee46de7cec59b9843e36f8468681cef3d
Akbar <akbar@cybertekschool.com>
2021-09-28 15:47:57 -0400

    Day 11 : testing List<WebElement> example for header row cells

--- src/test/java/com/cybertek/pages/WAllOrderPage.java
+++ src/test/java/com/cybertek/pages/WAllOrderPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class WAllOrderPage {
 
     @FindBy(xpath = "//h2[normalize-space(.)= 'List of All Orders']" )
@@ -16,8 +18,15 @@ public class WAllOrderPage {
     @FindBy(id = "ctl00_MainContent_btnUncheckAll")
     public WebElement unCheckAllButton ;
 
+    @FindBy(xpath = "//table[@class='SampleTable']/tbody/tr[1]/th")
+    public List<WebElement> headerCells ;
+
     public WAllOrderPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    // Create a method to loop through all header cell
+    // return true if the headers are as below false if not
+    // 	Name	Product	 #	Date	Street	City State	Zip	Card	Card Number	Exp
 
 }
