8d23867cb8ee23e72ede5008c9242f14daa88758
Akbar <akbar@cybertekschool.com>
2021-09-28 10:43:58 -0400

    Day 10 : Driver util update for reading browser from properties file

--- src/test/java/com/cybertek/utility/Driver.java
+++ src/test/java/com/cybertek/utility/Driver.java
@@ -3,6 +3,7 @@ package com.cybertek.utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * We wanted to have a class with that only return Single object
@@ -20,13 +21,30 @@ public class Driver {
      * @return same WebDriver if exists , new one if null
      */
     public static WebDriver getDriver(){
+        // read the browser type you want to launch from properties file
+        String browserName = ConfigReader.read("browser") ;
 
         if(obj == null){
-            WebDriverManager.chromedriver().setup();
-            obj = new ChromeDriver();
-            obj.manage().window().maximize();
-//            System.out.println("One and only created for the first time");
+
+            // according to browser type set up driver correctly
+            switch (browserName ){
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    obj = new ChromeDriver();
+                    break;
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    obj = new FirefoxDriver();
+                    break;
+                // other browsers omitted
+                default:
+                    obj = null ;
+                    System.out.println("UNKNOWN BROWSER TYPE!!! " + browserName);
+            }
             return obj ;
+
+
+
         }else{
 //            System.out.println("You have it just use existing one");
             return obj ;
