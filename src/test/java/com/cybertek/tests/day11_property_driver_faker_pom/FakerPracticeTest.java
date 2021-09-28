db2b6cf0195e8274afc6062da5dc7a3b8ac12876
Akbar <akbar@cybertekschool.com>
2021-09-28 11:48:35 -0400

    Day 10 : Faker Practice and homework description

---
+++ src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/FakerPracticeTest.java
@@ -0,0 +1,43 @@
+package com.cybertek.tests.day11_property_driver_faker_pom;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+public class FakerPracticeTest {
+
+    @Test
+    public void testPrintFakeData(){
+
+        Faker faker = new Faker();
+
+        // faker.YourCategory().WhateverAvailable() ;
+        System.out.println("faker.name().fullName() = "
+                        + faker.name().fullName());
+
+        // fake book info
+        System.out.println("faker.book().title() = "
+                + faker.book().title());
+
+        // fake phone number
+        System.out.println("faker.phoneNumber().cellPhone() = "
+                + faker.phoneNumber().cellPhone());
+
+        System.out.println("faker.chuckNorris().fact() = "
+                + faker.chuckNorris().fact());
+
+
+        // what if you want random numbers in specific format like phone number
+        // 111-111-1111
+        System.out.println("faker.numerify(\"###-###-####\") = "
+                + faker.numerify("###-###-####"));
+
+        System.out.println("faker.finance().creditCard() = "
+                + faker.finance().creditCard());
+
+        // AS a homework , use random data to fill up practice site registration form
+        // use random data to fill up the address and payment info in WebOrder site
+
+    }
+
+
+}
