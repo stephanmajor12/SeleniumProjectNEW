51af508fe65dfda31b5cf98db3081c71f6a46a09
Akbar <akbar@cybertekschool.com>
2021-09-28 17:04:53 -0400

    Day 11 : Using Order Page for Order Flow

--- src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
+++ src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -1,11 +1,15 @@
 package com.cybertek.tests.day11_property_driver_faker_pom;
 
 import com.cybertek.pages.WAllOrderPage;
+import com.cybertek.pages.WCommonArea;
 import com.cybertek.pages.WLoginPage;
+import com.cybertek.pages.WOrderPage;
 import com.cybertek.utility.BrowserUtil;
 import com.cybertek.utility.Driver;
 import com.cybertek.utility.TestBase;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -57,6 +61,76 @@ public class WebOrderPOM_Test extends TestBase {
         assertEquals(13, allOrderPage.headerCells.size() );
 
     }
+
+
+    @Test
+    public void testCommonArea(){
+
+        WLoginPage loginPage = new WLoginPage();
+        loginPage.goTo();
+        loginPage.login("Tester","test");
+        // --- here we logged in
+
+        WCommonArea commonArea = new WCommonArea() ;
+
+        commonArea.orderTab.click();
+        BrowserUtil.waitFor(2);
+
+        commonArea.viewAllProductTab.click();
+        BrowserUtil.waitFor(2);
+
+        commonArea.viewAllOrderTab.click();
+        BrowserUtil.waitFor(2);
+
+        commonArea.logoutLink.click();
+        BrowserUtil.waitFor(3);
+
+
+
+
+    }
+
+    @Test
+    public void testOrderFlow(){
+
+        WLoginPage loginPage = new WLoginPage();
+        loginPage.goTo();
+        loginPage.login("Tester","test");
+        // --- here we logged in
+
+        WCommonArea commonArea = new WCommonArea() ;
+
+        commonArea.orderTab.click();
+        BrowserUtil.waitFor(2);
+
+        // place order with information
+
+        WOrderPage orderPage = new WOrderPage();
+
+        orderPage.quantityBox.sendKeys(Keys.BACK_SPACE + "10" );
+        orderPage.calculateButton.click();
+
+        Faker faker = new Faker() ;
+
+        orderPage.customerNameBox.sendKeys(   faker.name().fullName()  );
+        orderPage.streetBox.sendKeys(   faker.address().streetAddress()    );
+        orderPage.cityBox.sendKeys(   faker.address().cityName()  );
+        orderPage.stateBox.sendKeys(  faker.address().state()  );
+        orderPage.zipBox.sendKeys(  faker.numerify("#####"  ) );
+
+        orderPage.visaRadioButton.click();
+        orderPage.cardNumber.sendKeys( faker.numerify("##################" ) ) ;
+        orderPage.cardDate.sendKeys(faker.numerify("0#/0#"));
+
+        orderPage.processButton.click();
+
+        assertTrue(   orderPage.successMessage.isDisplayed()       );
+
+        BrowserUtil.waitFor(5);
+
+    }
+
+
 
 
 }
