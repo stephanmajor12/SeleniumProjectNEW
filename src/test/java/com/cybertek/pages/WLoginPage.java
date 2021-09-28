742533d93133d25f740283f81565252e2618d98c
Akbar <akbar@cybertekschool.com>
2021-09-28 12:47:11 -0400

    Day 11 : First Page Object class WLoginPage has been created

---
+++ src/test/java/com/cybertek/pages/WLoginPage.java
@@ -0,0 +1,34 @@
+package com.cybertek.pages;
+
+import com.cybertek.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WLoginPage {
+
+    @FindBy(id = "ctl00_MainContent_username")
+    public WebElement userNameField ;
+
+    @FindBy(name = "ctl00$MainContent$password")
+    public WebElement passwordField ;
+
+    @FindBy(css = "#ctl00_MainContent_login_button")
+    public WebElement loginButton ;
+
+    // Now we need instruct selenium to start looking for element
+    // when this constructor is called
+    // This is no arg constructor of this class
+    public WLoginPage(){
+       // PageFactory is a selenium class that support Page Object Model
+        // and it has method called initElements
+        // once it's called , it will locate all the element
+        // specified using @FindBy annotation with locator
+        // initElements accept 2 arguments ,
+        // WebDriver instance and Page class instance (this) means current instance of this class.
+        PageFactory.initElements(Driver.getDriver() , this );
+
+    }
+
+
+}
