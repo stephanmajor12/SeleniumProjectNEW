ef4a4e18ab9e0ccf222e2926ad07dea8b50bebf1
Akbar <akbar@cybertekschool.com>
2021-09-28 13:05:14 -0400

    Day 11 : Trying out the PageObject Class WLogin

---
+++ src/test/java/com/cybertek/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -0,0 +1,26 @@
+package com.cybertek.tests.day11_property_driver_faker_pom;
+
+import com.cybertek.pages.WLoginPage;
+import com.cybertek.utility.BrowserUtil;
+import com.cybertek.utility.Driver;
+import com.cybertek.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class WebOrderPOM_Test extends TestBase {
+
+
+    @Test
+    public void testWithPOM_for_login(){
+
+        Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+        WLoginPage loginPage = new WLoginPage();
+
+        loginPage.userNameField.sendKeys("Tester");
+        loginPage.passwordField.sendKeys("test");
+        loginPage.loginButton.click();
+
+        BrowserUtil.waitFor(4);
+
+    }
+
+}
